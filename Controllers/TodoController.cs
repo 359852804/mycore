3e85703a8149b1954c78421c17379b8d2257bf2e
chenguoxin <359852804@qq.com>
2024-04-02 17:25:21 +0800

    添加数据处理

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,6 +2,7 @@
 using AspNetCoreTodo.Services;
 using AspNetCoreTodo.Models;
 using System.Threading.Tasks;
+using System;
 
 namespace AspNetCoreTodo.Controllers
 {
@@ -25,6 +26,39 @@ namespace AspNetCoreTodo.Controllers
             };
 
             return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.AddItemAsync(newItem);
+
+            if (!successful)
+            {
+                return BadRequest("添加失败");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> MarkDone(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+            var successful = await _todoItemService.MarkDoneAsync(id);
+            if (!successful)
+            {
+                return BadRequest("fail");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
